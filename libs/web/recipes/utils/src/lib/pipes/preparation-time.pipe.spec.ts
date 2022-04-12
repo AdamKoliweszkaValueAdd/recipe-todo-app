40640aced7fc8dc518d0e052950a33f9553f51a7
adamkoliweszka <adam.koliweszka@valueadd.pl>
2022-04-12 07:13:49 +0200

    refactor(utils): preparation pipe tests
    add preparation pipe tests

--- libs/web/recipes/utils/src/lib/pipes/preparation-time.pipe.spec.ts
+++ libs/web/recipes/utils/src/lib/pipes/preparation-time.pipe.spec.ts
@@ -5,4 +5,19 @@ describe('PreparationTimePipe', () => {
     const pipe = new PreparationTimePipe();
     expect(pipe).toBeTruthy();
   });
+
+  it('transforms "70" to "1h 10m"', () => {
+    const pipe = new PreparationTimePipe();
+    expect(pipe.transform(70)).toBe('1h 10m');
+  });
+
+  it('transforms "50" to "50m"', () => {
+    const pipe = new PreparationTimePipe();
+    expect(pipe.transform(50)).toBe('50m');
+  });
+
+  it('transforms "180" to "3h 0m"', () => {
+    const pipe = new PreparationTimePipe();
+    expect(pipe.transform(50)).toBe('3h 0m');
+  });
 });
