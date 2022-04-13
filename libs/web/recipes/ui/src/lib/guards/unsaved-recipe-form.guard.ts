4511bd58e8854309c8c8cbd9b7f014b21f6166ee
adamkoliweszka <adam.koliweszka@valueadd.pl>
2022-04-13 08:28:50 +0200

    refactor(app): libs dependencies
    move to other libs because of dependecies rules

--- libs/web/recipes/ui/src/lib/guards/unsaved-recipe-form.guard.ts
+++ libs/web/recipes/ui/src/lib/guards/unsaved-recipe-form.guard.ts
@@ -1,10 +1,10 @@
 import {Injectable} from "@angular/core";
 import {CanDeactivate} from "@angular/router";
-import {RecipeFormContentComponent} from "@recipes/ui";
+import {RecipeFormContentComponent} from "../components/recipe-form-content/recipe-form-content.component";
 
 @Injectable()
-export class UnsavedRecipeFormGuard implements CanDeactivate<{recipeFormContentComponent: RecipeFormContentComponent}> {
-  canDeactivate(component: {recipeFormContentComponent: RecipeFormContentComponent}): boolean {
+export class UnsavedRecipeFormGuard implements CanDeactivate<{ recipeFormContentComponent: RecipeFormContentComponent }> {
+  canDeactivate(component: { recipeFormContentComponent: RecipeFormContentComponent }): boolean {
     console.log(component)
     if (component.recipeFormContentComponent.recipeForm.dirty) {
       return confirm('Czy jesteś pewny, że chcesz opuścić stronę przed zapisaniem zmian?');
